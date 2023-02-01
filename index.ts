7288adab66432659a7a69858901d70681bbe2ac1
Anatole Dufour <konixy.p@gmail.com>
2023-02-01 14:21:26 +0100

    first commit

---
+++ index.ts
@@ -0,0 +1,59 @@
+import express from "express";
+import fileUpload, { UploadedFile } from "express-fileupload";
+import cors from "cors";
+// @ts-ignore
+import fileManager from "express-file-manager";
+import config from "./config";
+const app = express();
+
+app.use(cors());
+
+app.use(
+  fileUpload({
+    limits: {
+      fileSize: 10000000,
+    },
+    abortOnLimit: true,
+  })
+);
+
+app.use("/uploads", express.static(__dirname + "/uploads"));
+app.use("/uploads", fileManager(__dirname + "/uploads"));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.redirect(`/public/index.html`);
+});
+
+app.post("/upload", (req, res) => {
+  const files = req.files;
+
+  if (!files || !files.file)
+    return res.send({ success: false, message: "no files provided" });
+
+  const file = files.file as UploadedFile;
+
+  if (!file.mimetype.match(/^(image|application\/octet-stream)/)) {
+    return res.send({
+      success: false,
+      message: "this type of file is not allowed",
+    });
+  }
+  const newFileName = file.name.replace(/\s/g, "_");
+  file.mv(`${__dirname}/${config.saveDir}/${newFileName}`).then(
+    () => {
+      return res.send({
+        success: true,
+        url: `/${config.saveDir}/${newFileName}`,
+      });
+    },
+    (e) => {
+      console.log(e);
+      return res.send({ success: false, message: e.message });
+    }
+  );
+});
+
+app.listen(config.port, () => {
+  console.log(`Server running on port ${config.port}`);
+});
